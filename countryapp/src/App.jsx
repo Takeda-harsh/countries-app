8447c216f8ebe1269b37303b7732f0a9c108e777
Kofi Akyea <kakyea13@gmail.com>
2024-01-19 21:31:26 +0000

    added header and filter components

--- countryapp/src/App.jsx
+++ countryapp/src/App.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from "react"
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import Countries from "./components/countries"
+import Header from "./components/header"
+import Filter from "./components/filter"
 
 function App() {
   return (
     <>
+      <Header/>
+      <Filter/>
       <Countries/>
     </>
   )
---
+++ countryapp/src/components/header.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div>
+                    <h1>Where in the world?</h1>
+                </div>
+                <div>
+                    <i className="fas fa-moon"></i> Dark Mode
+                </div>
+                
+            </header>
+        </>
+    )
+}
+
+export default Header
